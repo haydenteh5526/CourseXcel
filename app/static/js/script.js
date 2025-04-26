d8f62c2177ce654093edae44ce8d83c3edde4d5c
poohbae <ameliadavid5687@yahoo.com>
2025-04-26 23:19:53 +0800

    update

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,13 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const togglePassword = document.querySelector('#togglePassword');
-    const password = document.querySelector('#password');
-
-    togglePassword.addEventListener('click', function () {
-        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-        password.setAttribute('type', type);
-        this.textContent = type === 'password' ? 'Show' : 'Hide';
-    });
-    
     const courseFormsContainer = document.getElementById('courseFormsContainer');
     const addCourseBtn = document.getElementById('addCourseBtn');
     const submitAllBtn = document.getElementById('submitAllBtn');
