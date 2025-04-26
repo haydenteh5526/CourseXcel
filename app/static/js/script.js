1deb30d541c9e6b2ecd4ab8932b75411739621c9
poohbae <ameliadavid5687@yahoo.com>
2025-04-26 23:04:44 +0800

    Add view/hide password feature

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#password');
+
+    togglePassword.addEventListener('click', function () {
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+        this.textContent = type === 'password' ? 'Show' : 'Hide';
+    });
+    
     const courseFormsContainer = document.getElementById('courseFormsContainer');
     const addCourseBtn = document.getElementById('addCourseBtn');
     const submitAllBtn = document.getElementById('submitAllBtn');
