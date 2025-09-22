7f756e4bb2d9071426c4f9da9823b30a541d17b0
poohbae <ameliadavid5687@yahoo.com>
2025-09-22 21:53:36 +0800

    update

--- app/auth.py
+++ app/auth.py
@@ -4,16 +4,23 @@ from flask import session
 
 def login_user(email, password):
     for role, model, id_key, email_key in [
-        ('admin', Admin, 'admin_id', 'admin_email'),
-        ('program_officer', ProgramOfficer, 'po_id', 'po_email'),
-        ('lecturer', Lecturer, 'lecturer_id', 'lecturer_email')
+        ('admin', Admin, 'admin_id', 'email'),
+        ('program_officer', ProgramOfficer, 'po_id', 'email'),
+        ('lecturer', Lecturer, 'lecturer_id', 'email')
     ]:
         # dynamically filter by correct email column
         user = model.query.filter(getattr(model, email_key) == email).first()
         if user and bcrypt.check_password_hash(user.password, password):
             session.permanent = True
             session[id_key] = getattr(user, id_key)
-            session[email_key] = getattr(user, email_key)
+
+            # assign correct session email key
+            if role == 'admin':
+                session['admin_email'] = getattr(user, email_key)
+            elif role == 'program_officer':
+                session['po_email'] = getattr(user, email_key)
+            elif role == 'lecturer':
+                session['lecturer_email'] = getattr(user, email_key)
+
             return role
     return None
-
